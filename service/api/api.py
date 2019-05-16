e5b9f49450b7ff86a8e241761f7c2742d87753a8
Kirill Zemskov <kzyemskov1@gmail.com>
2019-05-16 03:16:31 +0300

    Чуть улучшил ошибки

--- service/api/api.py
+++ service/api/api.py
@@ -7,14 +7,3 @@ log = logging.getLogger(__name__)
 api = Api(version='1.0')
 
 RESULT_OK = {'result': '0'}
-RESULT_NOT_FOUND = {'error': 'not-found'}
-RESULT_DELETED = {'error': 'deleted'}
-RESULT_ACTION_NOT_FOUND = {'error': 'action-not-found'}
-
-
-@api.errorhandler
-def default_error_handler(e):
-    message = 'An unhandled exception occurred.'
-    log.exception(message)
-
-    return {'message': message}, 500
--- service/api/endpoints.py
+++ service/api/endpoints.py
@@ -1,7 +1,7 @@
 from flask import jsonify, request
 from flask_restplus import Resource, reqparse
 
-from service.api.api import api, RESULT_NOT_FOUND, RESULT_OK, RESULT_DELETED, RESULT_ACTION_NOT_FOUND
+from service.api.api import api, RESULT_OK
 from service.db.models.server_rack import ServerRack
 from ..db.models.server import Server, ServerStatus
 
@@ -13,6 +13,8 @@ action_reqparser = reqparse.RequestParser()
 action_reqparser.add_argument('action', type=str, required=True, location=['json'])
 expiration_date_reqparser = reqparse.RequestParser()
 expiration_date_reqparser.add_argument('expirationDate', type=int, required=True, location=['json'])
+is_big_reqparser = reqparse.RequestParser()
+is_big_reqparser.add_argument('isBig', type=bool, default=False)
 
 
 @v1_namespace.route('/servers')
@@ -40,23 +42,23 @@ class ServerAPI(Resource):
         if server:
             return jsonify(server.to_dict())
         else:
-            return RESULT_NOT_FOUND, 404
+            v1_namespace.abort(404, 'Server not found')
 
     def put(self, id):
         print(id)
         server = Server.query.get(id)
         if server:
             if server.status == ServerStatus.DELETED:
-                return jsonify(RESULT_DELETED), 400
+                return v1_namespace.abort(4040, 'Server deleted')
             action = action_reqparser.parse_args(request).get('action')
             if action == 'pay':
                 expiration_date = expiration_date_reqparser.parse_args(request).get('expirationDate')
                 server.action_pay(expiration_date)
             else:
-                return jsonify(RESULT_ACTION_NOT_FOUND), 400
+                return v1_namespace.abort(400, 'Invalid action with server')
             return jsonify(RESULT_OK)
         else:
-            return RESULT_NOT_FOUND, 404
+            v1_namespace.abort(404)
 
     def delete(self, id):
         server = Server.query.get(id)
@@ -65,9 +67,7 @@ class ServerAPI(Resource):
         return jsonify(RESULT_OK)
 
 
-v1_namespace.route('/serverRacks')
-
-
+@v1_namespace.route('/serverRacks')
 class ServerRacksAPI(Resource):
 
     def get(self):
@@ -92,7 +92,7 @@ class ServerRackAPI(Resource):
         if server_rack:
             return jsonify(server_rack.to_dict())
         else:
-            return RESULT_NOT_FOUND, 404
+            return v1_namespace.abort(404, 'Server rack not found')
 
     def put(self, id):
         return jsonify()
